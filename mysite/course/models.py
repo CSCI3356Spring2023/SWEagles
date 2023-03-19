b158af7760dcf17f8bfa2a8f55e83d2d312b55fe
Christopher Siang <christophersiang@Christophers-MacBook-Pro-3.local>
2023-03-19 19:31:18 -0400

    course creation page edits

--- mysite/course/models.py
+++ mysite/course/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Course(models.Model):
+class AddCourse(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     instructor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
--- mysite/course/views.py
+++ mysite/course/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import AddCourse
 
 # Create your views here.
 

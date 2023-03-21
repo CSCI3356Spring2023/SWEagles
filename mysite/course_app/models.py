9c247a8b6940feace1e6f44a45c9c70717821cfc
Kevin Ciardelli <ciardelk@bc.edu>
2023-03-21 19:30:53 -0400

    adding everything

--- mysite/course_app/models.py
+++ mysite/course_app/models.py
@@ -3,12 +3,12 @@ from django import forms
 
 
 class courseApplication(models.Model):
-    First_and_Last_Name = models.CharField(max_length=100, blank=True)
-    pdf_file = forms.FileField()
     COURSE1 = "Monday 6-6:50, CSCI 1101.01.01"
     COURSE2= "Tuesday 6-6:50, CSCI 1101.01.02"
     COURSE3= "Wednesday 2-2:50, CSCI 1101.01.03"
     Course_Choices = [(COURSE1,"Discussion Times: Monday 6-6:50"), (COURSE2, "Discussion Times: Tuesday 6-6:50"), (COURSE3, "Discussion Times: Wednesday 2-2:50")]
-    courses = models.CharField(max_length=100, choices = Course_Choices)
-
-# Create your models here.
+    Discussion_Time = models.CharField(max_length=100, choices = Course_Choices)
+    First_and_Last_Name = models.CharField(max_length=100, blank=True)
+    Resume = models.FileField()
+    Cover_Letter = models.FileField()
+    Reference_Letter = models.FileField()
--- mysite/course_app/views.py
+++ mysite/course_app/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import courseForm
 
-def course_app_page(request):
-    form = courseForm()
+def course_app_view(request):
     if request.method == 'POST':
         form = courseForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('my_view_success')
     else:
         form = courseForm()
-
-
-
     return render(request, 'course_app_page.html', {'form': form})

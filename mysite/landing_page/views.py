c11cd3a767979634a588a3286352a9bae62f01aa
Christopher Siang <christophersiang@Christophers-MacBook-Pro-3.local>
2023-03-21 18:31:00 -0400

    adding student landing page to urls

--- mysite/landing_page/views.py
+++ mysite/landing_page/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request, 'landing.html')
 
+def student_landing_page(request):
+    return render(request, 'student_landing_page.html')

bab1b0b70d57f70d16ba5e33ebed38d26612d2f7
Kevin Ciardelli <ciardelk@bc.edu>
2023-03-21 20:32:09 -0400

    Adding multiple discussions

--- mysite/course_app/forms.py
+++ mysite/course_app/forms.py
@@ -4,4 +4,4 @@ from .models import courseApplication
 class courseForm(forms.ModelForm):
     class Meta:
         model = courseApplication
-        fields = ('Discussion_Time', 'First_and_Last_Name', 'Resume', 'Cover_Letter', 'Reference_Letter')
+        fields = ('Discussion_Time_1', 'Discussion_Time_2', 'First_and_Last_Name', 'Resume', 'Cover_Letter', 'Reference_Letter')
--- mysite/course_app/models.py
+++ mysite/course_app/models.py
@@ -6,8 +6,9 @@ class courseApplication(models.Model):
     COURSE1 = "Monday 6-6:50, CSCI 1101.01.01"
     COURSE2= "Tuesday 6-6:50, CSCI 1101.01.02"
     COURSE3= "Wednesday 2-2:50, CSCI 1101.01.03"
-    Course_Choices = [(COURSE1,"Discussion Times: Monday 6-6:50"), (COURSE2, "Discussion Times: Tuesday 6-6:50"), (COURSE3, "Discussion Times: Wednesday 2-2:50")]
-    Discussion_Time = models.CharField(max_length=100, choices = Course_Choices)
+    Course_Choices = [(COURSE1,"Monday 6-6:50"), (COURSE2, "Tuesday 6-6:50"), (COURSE3, "Wednesday 2-2:50")]
+    Discussion_Time_1 = models.CharField(max_length=100, choices = Course_Choices)
+    Discussion_Time_2 = models.CharField(max_length=100, choices = Course_Choices)
     First_and_Last_Name = models.CharField(max_length=100, blank=True)
     Resume = models.FileField()
     Cover_Letter = models.FileField()

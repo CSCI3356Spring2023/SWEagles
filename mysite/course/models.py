e89e631aab21e9e70bc93e4d2039f5097dbdfe64
Christopher Siang <christophersiang@Christophers-MacBook-Pro-3.local>
2023-04-02 18:13:51 -0400

    modifying fields for add course page

--- mysite/course/models.py
+++ mysite/course/models.py
@@ -1,14 +1,26 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 # Create your models here.
-
+DAYS_OF_WEEK = (
+        ('mon', 'Monday'),
+        ('tue', 'Tuesday'),
+        ('wed', 'Wednesday'),
+        ('thu', 'Thursday'),
+        ('fri', 'Friday'),
+        ('sat', 'Saturday'),
+        ('sun', 'Sunday'),
+    )
 class AddCourseModel(models.Model):
+    Instructor_ID = models.CharField(max_length=10, blank=True)
     Course_Name = models.CharField(max_length=100, blank=True)
     Course_ID = models.CharField(max_length=100, blank=True)
     Section = models.CharField(max_length=10, blank=True)
-    Marking_Meetings = models.CharField(max_length=10, blank=True)
-    Lecture_Times= models.CharField(max_length=100, blank=True)
-    Discussion_Times = models.CharField(max_length=100, blank=True)
+    Marking_Meetings = models.BooleanField(max_length=10, blank=True)
+    Lecture_Days = MultiSelectField(choices=DAYS_OF_WEEK, max_choices=3, max_length=3, blank= True)
+    Lecture_Times= models.TimeField(auto_now=False, auto_now_add=False)
+    Discussion_Days = MultiSelectField(choices=DAYS_OF_WEEK, max_choices=3, max_length=3, blank= True)
+    Discussion_Times = models.DurationField(max_length=100, blank=True)
     Course_Description = models.TextField(blank=True)
     Office_Hours_per_week = models.PositiveIntegerField(blank=True, null=True)
     TA_Positions = models.PositiveIntegerField(blank=True, null=True)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'multiselectfield',
     'crispy_forms',
     'crispy_bootstrap4',
     'main.apps.MainConfig',

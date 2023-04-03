e39edceda3ea85a9251ae5657e0f0b45777a717e
ananyabarthakur <ananyamsvv@gmail.com>
2023-04-03 19:57:20 -0400

    view app adjust to modified course app model

--- mysite/view_applications/views.py
+++ mysite/view_applications/views.py
@@ -3,13 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 #from .models import ToDoList, Item
 #from .forms import CreateNewList
 # Create your views here.
-from course_app.models import  courseApplication
+from course_app.models import courseApplicationAllRequired
 
 # Create your views here.
 
 def view_applications(request, custom_attribute):
     current_user = request.user
-    application_list =  courseApplication.objects.all()
+    application_list =  courseApplicationAllRequired.objects.all()
     application_list = application_list.filter(Course_Name=custom_attribute)
     
     return render(request, 'view_applications.html', {'custom_attribute': application_list})

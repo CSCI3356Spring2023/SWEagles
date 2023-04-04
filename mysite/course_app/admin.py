5cd44ed6ed5a026bbd5efad9e7015592e51dbdac
Kevin Ciardelli <ciardelk@bc.edu>
2023-04-04 02:25:42 -0400

    Adding App Limit

--- mysite/course_app/admin.py
+++ mysite/course_app/admin.py
@@ -3,9 +3,15 @@ from .forms import *
 from .models import *
 
 
-class CustomCourseAdmin(admin.ModelAdmin):
+class CustomCourseAdmin1(admin.ModelAdmin):
     form = courseForm1
     readonly_fields = ('Course_Name','Course_Section',)
     list_display = ('Course_Name', 'Course_Section', 'Discussion_Time_1', 'Discussion_Time_2', 'First_and_Last_Name', 'Resume', 'Cover_Letter', 'Reference_Letter')
 
-admin.site.register(courseApplicationAllRequired, CustomCourseAdmin)
+class CustomCourseAdmin2(admin.ModelAdmin):
+    form = courseForm2
+    readonly_fields = ('Course_Name','Course_Section',)
+    list_display = ('Course_Name', 'Course_Section', 'Discussion_Time_1', 'Discussion_Time_2', 'First_and_Last_Name', 'Resume', 'Cover_Letter', 'Reference_Letter')
+
+admin.site.register(courseApplicationAllRequired, CustomCourseAdmin1)
+admin.site.register(courseApplicationOnlyResume, CustomCourseAdmin2)
--- mysite/course_app/views.py
+++ mysite/course_app/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from .forms import *
-from django.http import HttpResponse
+from django.shortcuts import redirect
 from course.models import AddCourseModel
+from register.models import StudentUser
 from .models import *
+from django.http import HttpResponse
 
-def course_app_view(response, course_id):
+def course_app_view(response, course_id, current_user_id):
+
 
     data = AddCourseModel.objects.get(id=course_id)
+    student = StudentUser.objects.get(id=current_user_id)
+    #print(student.app_counter)
+    if(student.app_counter > 4):
+        return HttpResponse('you have applied to the maximum amount of applications!')
 
     Required = data.Cover_Letter
     Course_Name = data.Course_Name
     Course_Section = data.Section
-    print(Required)
+    #print(Required)
     if(Required == 'required'):
         form = courseForm1()
         if response.POST:
@@ -21,13 +28,16 @@ def course_app_view(response, course_id):
                 application = form.save(commit=False)
                 application.Course_Name = Course_Name
                 application.Section = Course_Section
+                student.app_counter+=1
                 application.save()
-                return HttpResponse(Course_Name)
+                student.save()
+                return redirect('student_landing_page')
 
         else:
             form = courseForm1()
 
         context = {
+            'student' : student,
             'course' : data,
             'form' : form
         }
@@ -43,13 +53,15 @@ def course_app_view(response, course_id):
                 application = form.save(commit=False)
                 application.Course_Name = Course_Name
                 application.Section = Course_Section
+                student.app_counter+=1
                 application.save()
-                return HttpResponse(Course_Name)
+                return redirect('student_landing_page')
 
         else:
             form = courseForm2()
 
         context = {
+            'student' : student,
             'course' : data,
             'form' : form
         }

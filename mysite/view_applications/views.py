371ed28c7760a09d0af8f4de58a7182932b2ac05
ananyabarthakur <ananyamsvv@gmail.com>
2023-04-03 20:23:15 -0400

    view_application modify for multi courseapp models

--- mysite/view_applications/views.py
+++ mysite/view_applications/views.py
@@ -1,17 +1,26 @@
+from itertools import chain
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 #from .models import ToDoList, Item
 #from .forms import CreateNewList
 # Create your views here.
-from course_app.models import courseApplicationAllRequired
+from course_app.models import courseApplicationAllRequired, courseApplicationOnlyResume
 
 # Create your views here.
 
 def view_applications(request, custom_attribute):
     current_user = request.user
-    application_list =  courseApplicationAllRequired.objects.all()
-    application_list = application_list.filter(Course_Name=custom_attribute)
+    # get applications with file 
+    application_list_1 =  courseApplicationAllRequired.objects.all()
+    application_list_1 = application_list_1.filter(Course_Name=custom_attribute)
     
-    return render(request, 'view_applications.html', {'custom_attribute': application_list})
+    # get file optional applications
+    application_list_2 =  courseApplicationOnlyResume.objects.all()
+    application_list_2 = application_list_2.filter(Course_Name=custom_attribute)
+    
+    # append sets
+    combined_applications = chain(application_list_1, application_list_2)
+    
+    return render(request, 'view_applications.html', {'custom_attribute': combined_applications})
 
 

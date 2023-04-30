3920d6e7c63a91d64970268c70d76cc4a071220b
asikarov <37193177+asikarov@users.noreply.github.com>
2023-04-30 14:21:35 -0400

    fixed bug
    made list of usernames from queryset of users

--- mysite/view_applications/views.py
+++ mysite/view_applications/views.py
@@ -5,19 +5,19 @@ from django.http import HttpResponse, HttpResponseRedirect
 #from .forms import CreateNewList
 # Create your views here.
 from course_app.models import courseApplicationAllRequired
-from login.models import CustomUserBackend
+
 from register.models import StudentUser
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def view_applications(request, custom_attribute):
     unhired_users = StudentUser.objects.filter(hire_status = 'unhired')
+    unhired_users = [user.username for user in unhired_users]
     current_user = request.user
     # get applications with file 
     application_list_1 =  courseApplicationAllRequired.objects.all()
-    application_list_1 = application_list_1.filter(Course_Name=custom_attribute)
     application_list_1 = application_list_1.filter(Username__in=unhired_users)
-    application_list_1 = StudentUser.objects.all()
     
     
     # append sets

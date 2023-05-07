85faeffa8a81e1ddb2bbd9b7664c6700ebd3902a
ananyabarthakur <ananyamsvv@gmail.com>
2023-05-07 17:02:33 -0400

    only show positioned application once hired

--- mysite/student_view_appplications/views.py
+++ mysite/student_view_appplications/views.py
@@ -11,12 +11,18 @@ from register.models import StudentUser
 
 def view_applications(request):
     current_user = request.user.username
+    
     # get applications with file 
     application_list_1 =  courseApplicationAllRequired.objects.all()
     application_list_1 = application_list_1.filter(Username=current_user)
+    applications = [] 
     
+    # if student is hired, only show positioned application
+    for application in application_list_1:
+        if application.Application_Status == "Accepted_by_both":
+            applications.append(application)
+            return render(request, 'student_view_applications.html', {'custom_attribute': applications})
     
-    # append sets
-    combined_applications = application_list_1
-    
-    return render(request, 'student_view_applications.html', {'custom_attribute': combined_applications})
+    # otherwise show all applications    
+    return render(request, 'student_view_applications.html', {'custom_attribute': application_list_1})
+            
